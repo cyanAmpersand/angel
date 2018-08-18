2f7966b9b4224f68b7409b8473a6d02d041388df
Chloe Horridge <nickh95@live.co.uk>
2018-08-19 00:40:31 +0100

    snapture now kills 50% every time

--- sillystuff.py
+++ sillystuff.py
@@ -1,21 +1,44 @@
 import random
-import botfunctions
+import copy
+
+def n_dead_members(snapinfo):
+    n = 0
+    for m in snapinfo:
+        if not snapinfo[m]["alive"]:
+            n += 1
+    return n
 
 def infinitysnap(members):
-    non_bots = []
+    users = []
+    snapture_info = {}
+    base_status = {
+        "alive":True,
+        "member_obj":None
+    }
+    total_users = 0
     for m in members:
         if not m.bot:
-            non_bots.append(m)
+            users.append(m)
+            snapture_info[m.id] = copy.copy(base_status)
+            snapture_info[m.id]["member_obj"] = m
+            total_users += 1
+    random.shuffle(users)
     deaths = 0
+    n = 0
+    for m in snapture_info:
+        if n_dead_members(snapture_info) < total_users/2:
+            snapture_info[m]["alive"] = False
+    if total_users%2 != 0:
+        print("odd correction")
+        snapture_info[random.choice(users).id]["alive"] = True
     response = "```"
-    for m in non_bots:
-        name = m.name
-        if (random.random() > 0.5 or m.id == "125424672765509632") and m.id != "190065758267637760":
-            response += name + " was killed.\n"
+    for m in users:
+        if not snapture_info[m.id]["alive"]:
+            response += m.name + " was killed.\n"
             deaths += 1
         else:
-            response += name + " was spared.\n"
-    response += str(deaths) + "/" + str(len(non_bots)) + " users were killed. The server is now balanced, as all things should be.```"
+            response += m.name + " was spared.\n"
+    response += str(deaths) + "/" + str(total_users) + " users were killed. The server is now balanced, as all things should be.```"
     return response
 
 def infinitysnaptest(usernames):
